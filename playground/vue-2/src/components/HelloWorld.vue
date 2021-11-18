ab7d44e8fe89e1555094e43990989fa6e927786d
Daragh King <daragh.king@genesys.com>
2021-11-18 15:42:25 +0000

    test(vue-2): new bug reproduction
    new bug reproduction
    
    COMUI-722

--- playground/vue-2/src/components/HelloWorld.vue
+++ playground/vue-2/src/components/HelloWorld.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="container">
+      <div lang="en">
+    <div class="table-placeholder"></div>
+    <gux-pagination :total-items="totalItems" items-per-page="25"></gux-pagination>
+  </div>
+
     <gux-form-field>
       <input
         slot="input"
-        name="errorInput"
-        type="text"
-        placeholder="Enter text"
+        name="number"
+        type="number"
+        placeholder="Enter number"
+        step="50"
+        min="0"
+        v-model.number="totalItems"
       />
-      <label slot="label">Label</label>
-      <span slot="error" v-if="hasError">Field is required</span>
+      <label slot="label">Number</label>
     </gux-form-field>
 
-    <gux-button v-on:click="toggleHasError">Toggle Has Error</gux-button>
-    <div>Has Error: {{hasError}}</div>
+    <gux-button v-on:click="doubleTotalItems">Double Total Items</gux-button>
   </div>
 </template>
 
@@ -21,12 +27,12 @@ export default {
   name: 'HelloWorld',
   data: function () {
     return {
-      hasError: false
+      totalItems: 100
     }
   },
   methods: {
-    toggleHasError: function () {
-      this.hasError = !this.hasError;
+    doubleTotalItems: function () {
+      this.totalItems = this.totalItems * 2;
     }
   }
 }
@@ -36,5 +42,28 @@ export default {
   .container {
     border: 1px solid black;
     padding: 20px;
+  }
+
+  .table-placeholder {
+    padding: 10px;
+    color: #f1f1f1;
+    text-align: center;
+    background-image: linear-gradient(
+      45deg,
+      #f1f1f1 25%,
+      transparent 25%,
+      transparent 50%,
+      #f1f1f1 50%,
+      #f1f1f1 75%,
+      transparent 75%,
+      #fff
+    );
+    background-size: 5px 5px;
+  }
+
+  .table-placeholder::before {
+    padding: 5px;
+    content: 'Table Placeholder';
+    background-color: white;
   }
 </style>
